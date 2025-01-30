aaec78a4f1a5ff66f80e31e723a17a71038224f7
Pol Nadenoen <pol.nadenoen@seeben.com>
2025-01-30 14:15:35 +0100

    readme basic syntax

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ExpAsyncCPP
-A simple C++20 experimental async library 
+
+## A simple C++20 experimental async library 
 
 The repository aim to show a possible way to implement a asynchronous environment using coroutines.
 
@@ -19,4 +20,56 @@ This premitive is used to send a message threadsafely.
 
 UnboundedSPSC.h contains:\
 -Single producer, single consumer messaging premitive. Multiple elements can be sended.\
-This premitive is used to send a messages threadsafely.
+This premitive is used to send messages threadsafely.
+
+### Basic syntax
+
+```cpp
+#include <iostream>
+#include <Async.h>
+#include <AsyncExecutor.h>
+static async::AsyncCoroutine<int> subcoroutine() {
+    co_return 200;
+}
+static async::AsyncCoroutine<void> coroutine() {
+    std::cout << co_await subcoroutine() << std::endl;
+}
+int main()
+{   //block the thread until coroutine is finished.
+    async::BlockingExecutor().block_on(coroutine());
+}
+```
+
+Use co_return instead of return.\
+use co_await to wait an other AsyncCoroutine (or other premitive).\
+
+an async::AsyncCoroutine can be returned by a function, method or a virtual method.
+
+### Waiting for multiple Task
+
+The function async::join use used to wait multiple waitable.\
+Waiting async::join produce a tuple. 
+
+```cpp
+static async::AsyncCoroutine<int> subcoroutine() {
+    co_return 200;
+}
+static async::AsyncCoroutine<double> subcoroutine2() {
+    co_return 19.2;
+}
+static async::AsyncCoroutine<void> coroutine() {
+    auto result = co_await async::join(subcoroutine(), subcoroutine2());
+    std::cout << std::get<0>(result) << std::endl;
+    std::cout << std::get<1>(result) << std::endl;
+}
+int main()
+{
+    async::BlockingExecutor().block_on(coroutine());
+}
+```
+
+If the result isn't needed, async::join_task have the same behavior but waiting it produce void.  
+
+## Optimizations
+
+It may be slow... i dunno.
--- test/test.cpp
+++ test/test.cpp
@@ -1,68 +1,18 @@
 #include <iostream>
-#include <UnBoundedSPSC.h>
-#include <AsyncExecutor.h>
 #include <Async.h>
-#include <SingleShot.h>
-static async::AsyncCoroutine<void> co(async::SPSCReader<int>&& reader) {
-    std::vector<int> t= co_await async::utils::collect(std::move(reader));
-    for (int i : t) {
-        std::cout << "{ " << i << " }";
-    }
-    std::cout << std::endl;
-};
-
-static async::AsyncCoroutine<void> coss(async::SingleShotReader<int>&& reader) {
-    std::optional<int> result= co_await std::move(reader);
-    if (result) {
-        std::cout << "single shot found" << *result<<std::endl;
-    }
-    else {
-        std::cout << "writer deleted" << std::endl;
-    }
+#include <AsyncExecutor.h>
+static async::AsyncCoroutine<int> subcoroutine() {
+    co_return 200;
+}
+static async::AsyncCoroutine<double> subcoroutine2() {
+    co_return 19.2;
+}
+static async::AsyncCoroutine<void> coroutine() {
+    auto result = co_await async::join(subcoroutine(), subcoroutine2());
+    std::cout << std::get<0>(result) << std::endl;
+    std::cout << std::get<1>(result) << std::endl;
 }
 int main()
 {
-    auto spsc =  std::make_shared<async::UnBoundedSPSC<int>>();
-    auto spsc2 = std::make_shared<async::UnBoundedSPSC<int>>();
-    auto spsc3 = std::make_shared<async::UnBoundedSPSC<int>>();
-    auto singleShot = std::make_shared<async::SingleShot<int>>();
-    std::thread t1([writer = async::SPSCWriter<int>(spsc)]() mutable {
-        int i = 0;
-        while (i < 10) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            writer.push(i);
-            i++;
-        }
-    });
-    std::thread t2([writer = async::SPSCWriter<int>(spsc2)]() mutable {
-        int i = 0;
-        while (i < 15) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            writer.push(i);
-            i++;
-        }
-    });
-    std::thread t3([writer = async::SPSCWriter<int>(spsc3)]() mutable {
-        int i = 0;
-        while (i < 50) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            writer.push(i);
-            i++;
-        }
-        });
-    std::thread t4([writer = async::SingleShotWriter(singleShot)]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-        //writer.set_value(1);
-        });
-    async::BlockingExecutor().block_on([&spsc, &spsc2,&spsc3,&singleShot]()->async::AsyncCoroutine<void> {
-        co_await join_task(co(std::move(async::SPSCReader(spsc))),
-                           co(async::SPSCReader(spsc2)),
-                           co(async::SPSCReader(spsc3)),
-                           coss(async::SingleShotReader(singleShot))
-                           );
-        }());
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
+    async::BlockingExecutor().block_on(coroutine());
 }
